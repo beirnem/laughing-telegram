b7bb10c11cb9a1d8e16f6342a9b9640b3fe5b12f
Marcus Beirne <marcus.beirne@gmail.com>
2020-01-30 21:19:51 +0000

    Add the code to create the database, and CRUD functions in the database. Update Application start App.xaml.cs to create the DB.

--- Notes/Notes/App.xaml.cs
+++ Notes/Notes/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Notes.Data;
+using System;
 using System.IO;
 using Xamarin.Forms;
 
@@ -6,14 +7,47 @@ namespace Notes
 {
     public partial class App : Application
     {
-        public static string FolderPath { get; private set; }
+        static NoteDatabase database;
+
+        public static NoteDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new NoteDatabase(
+                        Path.Combine(
+                            Environment.GetFolderPath(
+                                Environment.SpecialFolder.LocalApplicationData
+                            ), "Notes.db3"));
+                }
+                return database;
+            }
+            
+        }
 
         public App()
         {
             InitializeComponent();
-            FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
             MainPage = new NavigationPage(new NotesPage());
         }
-        // ...
+
+        protected override void OnStart()
+        {
+            // Handle when the app starts
+            base.OnStart();
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when the app sleeps
+            base.OnSleep();
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when the app resumes
+            base.OnResume();
+        }
     }
 }
